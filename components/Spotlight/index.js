fd43ffab4ede90746feca3385a098efd5abbe9c9
Bruno <bruno.dill@protonmail.com>
2024-01-26 14:32:25 +0100

    centered Spotlight page and renamed it

--- components/Spotlight/index.js
+++ components/Spotlight/index.js
@@ -1,8 +1,17 @@
 import Image from "next/image";
+import styled from "styled-components";
+
+const FlexCenter = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  flex: 1;
+`;
 
 export default function Spotlight({ image }) {
   return (
-    <>
+    <FlexCenter>
       <Image
         src={image.imageSource}
         width={image.dimensions.width * 0.1}
@@ -10,6 +19,6 @@ export default function Spotlight({ image }) {
         alt={image.name}
       ></Image>
       <p>{image.artist}</p>
-    </>
+    </FlexCenter>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Layout from "@/components/Layout";
 import Spotlight from "@/components/Spotlight";
 import getRandomPicture from "@/lib/getRandomPicture";
 
-export default function HomePage({ pieces }) {
+export default function SpotlightPage({ pieces }) {
   return (
     <Layout>
       <Spotlight image={getRandomPicture(pieces)} />
