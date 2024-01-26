cec4245d82f4b0c6e8651785ae050562bb1fe5a3
Bruno <bruno.dill@protonmail.com>
2024-01-26 11:29:59 +0100

    added preview list

---
+++ components/ArtPieces/index.js
@@ -0,0 +1,20 @@
+import { nanoid } from "nanoid";
+import ArtPiecePreview from "../ArtPiecePreview";
+import styled from "styled-components";
+
+const List = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  gap: 2rem;
+  padding: 0;
+`;
+
+export default function ArtPieces({ pieces }) {
+  return (
+    <List>
+      {pieces.map((piece) => (
+        <ArtPiecePreview key={nanoid()} image={piece} />
+      ))}
+    </List>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,24 @@
+import useSWR from "swr";
+import { nanoid } from "nanoid";
+import ArtPieces from "@/components/ArtPieces";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
 export default function HomePage() {
+  const URL = "https://example-apis.vercel.app/api/art";
+  const { data, isLoading, error } = useSWR(URL, fetcher);
+
+  if (isLoading) {
+    return null;
+  }
+  if (error) {
+    return null;
+  }
+
   return (
     <div>
-      <h1>Hello from Next.js</h1>
+      <h1>Art Gallery</h1>
+      <ArtPieces pieces={data} />
     </div>
   );
 }
