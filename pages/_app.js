1d038bfd4df6ee90c2b60fe50a566e5d896d769d
MareikeFla <mwidderich@gmail.com>
2024-01-26 13:07:40 +0100

    Add routing via slug.js when clicking on a picture

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,22 @@
 import GlobalStyle from "../styles";
+import useSWR from "swr";
 
 export default function App({ Component, pageProps }) {
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+  const URL = "https://example-apis.vercel.app/api/art";
+  const { data, isLoading, error } = useSWR(URL, fetcher);
+
+  if (isLoading) {
+    return null;
+  }
+  if (error) {
+    return null;
+  }
   return (
     <>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <Component {...pageProps} pieces={data} />
     </>
   );
 }
--- pages/art-pieces/index.js
+++ pages/art-pieces/index.js
@@ -1,3 +1,20 @@
-{
-  /* <ArtPieces pieces={data} /> */
+import { nanoid } from "nanoid";
+import ArtPiecePreview from "@/components/ArtPiecePreview";
+import styled from "styled-components";
+
+const List = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  gap: 2rem;
+  padding: 0;
+`;
+
+export default function ArtPieces({ pieces }) {
+  return (
+    <List>
+      {pieces.map((piece) => (
+        <ArtPiecePreview key={nanoid()} image={piece} />
+      ))}
+    </List>
+  );
 }
