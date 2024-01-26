8c3b13349b57f73e53ca7a02adc9f856b15fcccf
Bruno <bruno.dill@protonmail.com>
2024-01-26 13:37:43 +0100

    styled Navigation

--- components/Navigation/index.js
+++ components/Navigation/index.js
@@ -1,11 +1,29 @@
 import Link from "next/link";
+import styled from "styled-components";
+
+const NavLinks = styled.nav`
+  display: flex;
+  justify-content: center;
+  gap: 2rem;
+  margin: 2rem;
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: inherit;
+  cursor: pointer;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
 
 export default function Navigation() {
   return (
-    <nav>
-      <Link href={"/"}>Spotlight</Link>
-      <Link href={"/art-pieces"}>Art Pieces</Link>
-      <Link href={"/favorites"}>Favorites</Link>
-    </nav>
+    <NavLinks>
+      <StyledLink href={"/"}>Spotlight</StyledLink>
+      <StyledLink href={"/art-pieces"}>Art Pieces</StyledLink>
+      <StyledLink href={"/favorites"}>Favorites</StyledLink>
+    </NavLinks>
   );
 }
