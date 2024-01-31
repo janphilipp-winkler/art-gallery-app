f9f7d3452bbc12e7729ed1602bdf46a802184f9b
Bruno <bruno.dill@protonmail.com>
2024-01-31 11:11:10 +0100

    added linear gradient to Header

--- components/ArtPieceDetailedInfo/index.js
+++ components/ArtPieceDetailedInfo/index.js
@@ -1,9 +1,32 @@
+import styled from "styled-components";
+
+const DetailsHeader = styled.h2`
+  font-size: 4rem;
+  background-image: linear-gradient(
+    45deg,
+    ${(props) => props.$bgColorOne},
+    ${(props) => props.$bgColorTwo},
+    ${(props) => props.$bgColorThree}
+  );
+  background-clip: text;
+  color: transparent;
+`;
+
 export default function ArtPieceDetailedInfo({ piece }) {
-  const { name, year, artist, genre, dimensions, slug } = piece;
+  const { name, year, artist, genre, dimensions, slug, colors } = piece;
   const { width, height, type } = dimensions;
+
+  console.log(colors);
+
   return (
     <>
-      <h2 style={{ fontSize: 60 }}>{name}</h2>
+      <DetailsHeader
+        $bgColorOne={colors[0]}
+        $bgColorTwo={colors[2]}
+        $bgColorThree={colors[colors.length - 1]}
+      >
+        {name}
+      </DetailsHeader>
       <p>Year: {year}</p>
       <p>Artist: {artist}</p>
       <p>Genre: {genre}</p>
