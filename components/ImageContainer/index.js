0b667c7aa60332a2d443c0bc60f4302a286ea848
MareikeFla <mwidderich@gmail.com>
2024-01-31 12:32:25 +0100

    add fav heart a little border and make it not red but with a nice gradient

--- components/ImageContainer/index.js
+++ components/ImageContainer/index.js
@@ -18,7 +18,7 @@ const Svg = styled.svg`
   padding: 0;
 `;
 
-const GridItemLinkBox = styled(Link)`
+const LinkBox = styled(Link)`
   text-decoration: none;
   color: black;
   margin: 0;
@@ -48,7 +48,7 @@ export default function ImageContainer({
   }
   return (
     <DivContainer>
-      <GridItemLinkBox href={href ? `art-pieces/${href}` : "#"}>
+      <LinkBox href={href ? `art-pieces/${href}` : "#"}>
         <Image
           id={id}
           src={src}
@@ -60,16 +60,32 @@ export default function ImageContainer({
           placeholder={placeholder ? placeholder : ""}
           opacity={opacity ? opacity : "100%"}
         />
-      </GridItemLinkBox>
+      </LinkBox>
       <Svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 24 24"
-        fill={favorites[id] === true ? "red" : "white"}
         onClick={(event) => {
           handleFavClick(id);
         }}
       >
-        <path d="m12 21.35-1.45-1.32C5.4 15.36 2 12.27 2 8.5 2 5.41 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.08C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.41 22 8.5c0 3.77-3.4 6.86-8.55 11.53L12 21.35Z" />
+        <defs>
+          <linearGradient id="shiny-red" x1="100%" y1="100%" x2="0%" y2="0%">
+            <stop
+              offset="0%"
+              style={{ stopColor: "rgb(255, 120, 220)", stopOpacity: 0.8 }}
+            />
+            <stop
+              offset="100%"
+              style={{ stopColor: "rgb(255, 0, 80)", stopOpacity: 1 }}
+            />
+          </linearGradient>
+        </defs>
+        <path
+          d="m12 21.35-1.45-1.32C5.4 15.36 2 12.27 2 8.5 2 5.41 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.08C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.41 22 8.5c0 3.77-3.4 6.86-8.55 11.53L12 21.35Z"
+          fill={favorites[id] === true ? "url(#shiny-red)" : "white"}
+          stroke={favorites[id] === true ? "" : "rgb(255, 120, 220)"}
+          strokeWidth="0.4"
+        />
       </Svg>
     </DivContainer>
   );
