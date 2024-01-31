a7026a1caac2edcfe6261b60336630e86f13091d
MareikeFla <mwidderich@gmail.com>
2024-01-31 13:52:08 +0100

    add router instead of art-pieces and favourite pages

---
+++ pages/[slug].js
@@ -0,0 +1,54 @@
+import { nanoid } from "nanoid";
+import ArtPiecePreview from "@/components/ArtPiecePreview";
+import { List } from "@/components/StyledComponents";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+
+export default function ArtPieces({ pieces, setFavorites, favorites }) {
+  const router = useRouter();
+  const { slug } = router.query;
+
+  const [likedPieces, setLikedPieces] = useState([]);
+
+  useEffect(() => {
+    const filteredPieces = pieces.filter((piece) => {
+      return favorites[piece.slug] === true;
+    });
+    setLikedPieces(filteredPieces);
+  }, [favorites]);
+
+  console.log(likedPieces);
+  if (slug === "art-pieces") {
+    return (
+      <List>
+        {pieces.map((piece) => (
+          <ArtPiecePreview
+            key={nanoid()}
+            image={piece}
+            favorites={favorites}
+            setFavorites={setFavorites}
+          />
+        ))}
+      </List>
+    );
+  }
+
+  if (slug === "favorites") {
+    return (
+      <List>
+        {likedPieces.map((piece) => (
+          <>
+            <ArtPiecePreview
+              key={nanoid()}
+              image={piece}
+              favorites={favorites}
+              setFavorites={setFavorites}
+            />
+          </>
+        ))}
+      </List>
+    );
+  } else {
+    router.push("/");
+  }
+}
