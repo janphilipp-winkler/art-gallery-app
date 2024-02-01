7398a4773b5f33142115ecb41bf1cde5dda48dfa
MareikeFla <mwidderich@gmail.com>
2024-02-01 11:42:04 +0100

    add message for user when no pictures are faved

--- pages/[slug].js
+++ pages/[slug].js
@@ -4,6 +4,7 @@ import { List } from "@/components/StyledComponents";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import useLocalStorageState from "use-local-storage-state";
+import Link from "next/link";
 
 export default function ArtPieces({ pieces, setFavorites, favorites }) {
   const router = useRouter();
@@ -36,20 +37,29 @@ export default function ArtPieces({ pieces, setFavorites, favorites }) {
   }
 
   if (slug === "favorites") {
-    return (
-      <List>
-        {likedPieces.map((piece) => (
-          <>
-            <ArtPiecePreview
-              key={nanoid()}
-              image={piece}
-              favorites={favorites}
-              setFavorites={setFavorites}
-            />
-          </>
-        ))}
-      </List>
-    );
+    if (likedPieces.length > 0) {
+      return (
+        <List>
+          {likedPieces.map((piece) => (
+            <>
+              <ArtPiecePreview
+                key={nanoid()}
+                image={piece}
+                favorites={favorites}
+                setFavorites={setFavorites}
+              />
+            </>
+          ))}
+        </List>
+      );
+    } else {
+      return (
+        <h3>
+          You have no favorite pictures yet, have a look at our{" "}
+          <Link href="/art-pieces">art pieces</Link> and like some!
+        </h3>
+      );
+    }
   } else {
     router.push("/");
   }
