d570c067a52759567288f86c2c7051c67c0703d5
MareikeFla <mwidderich@gmail.com>
2024-02-01 16:02:07 +0100

    remove outomented stuff

--- pages/_app.js
+++ pages/_app.js
@@ -3,15 +3,6 @@ import GlobalStyle from "../styles";
 import useSWR from "swr";
 import Layout from "@/components/Layout";
 import useLocalStorageState from "use-local-storage-state";
-import { useEffect } from "react";
-
-// // Assuming pictures is an array of picture infos
-// const pictures = [
-//   { slug: "" },
-//   // Add the rest of the picture infos here
-// ];
-
-// Render UI here
 
 export default function App({ Component, pageProps }) {
   const URL = "https://example-apis.vercel.app/api/art";
